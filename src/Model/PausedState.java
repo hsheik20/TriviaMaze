3760a6e4bb22e8df6b01c6aabb1343a20a350311
SAM-2nd <chant7@uw.edu>
2025-08-07 16:30:38 -0700

    update all gamestate, maze, player, question class to add my and make parameter declared final

--- src/Model/PausedState.java
+++ src/Model/PausedState.java
@@ -1,4 +1,12 @@
 package Model;
 
-public class PausedState {
+class PauseState extends GameState {
+    public void enter(GameStateManager manager) {
+        this.manager = manager;
+    }
+
+    public void exit() {}
+    public void update() {}
+    public String getStateName() { return "PAUSE"; }
 }
+
--- src/Model/PlayingState.java
+++ src/Model/PlayingState.java
@@ -1,4 +1,13 @@
 package Model;
 
-public class PlayingState {
+class PlayingState extends GameState {
+    public void enter(GameStateManager manager) {
+        this.manager = manager;
+        manager.getMaze().generateMaze();
+        manager.getPlayer().resetPosition();
+    }
+
+    public void exit() {}
+    public void update() {}
+    public String getStateName() { return "MAZE"; }
 }
